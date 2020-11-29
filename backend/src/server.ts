6f33c61804bec5ab8f329f6245c91de50b8f91dc
Gervásio Araújo <gervasfj@gmail.com>
2020-11-29 11:06:30 -0300

    indent backend code with eslint rules

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Node Server is running!' })
-})
+  return response.json({ message: 'Node Server is running!' });
+});
 
 app.listen(3333, () => {
-    console.log('Node server is running on port 3333... !')
-})
+  console.log('Node server is running on port 3333... !');
+});
